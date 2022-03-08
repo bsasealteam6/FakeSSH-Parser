d4ce0bbffa7145a2b4a9211fe48df388a0672ac4
Nate <nefortner@outlook.com>
2022-03-08 08:00:10 -0800

    Fixing if statement

--- src/FakeSSHParser.java
+++ src/FakeSSHParser.java
@@ -1,21 +1,28 @@
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class FakeSSHParser {
-    private ArrayList<String> lines;
-    private CsvList<SSHTry> sshTries;
-    public FakeSSHParser(String filename) {
+    private ArrayList < String > lines;
+    private CsvList < SSHTry > sshTries;
+    private String outputFilename;
+    public FakeSSHParser(String filename, String outputFilename) {
         this.lines = ReadWriteFile.readFile(filename);
-        sshTries = new CsvList<SSHTry>();
+        sshTries = new CsvList < SSHTry > ();
+        this.outputFilename = outputFilename;
     }
 
-    public boolean parse() {
+    public boolean parse() throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter file = new PrintWriter(outputFilename, "UTF-8");
+        file.println("Date,Time,IP,Port,Software,Username,Password");
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
-            if(
-                !line.contains("Disconnected")&&
-                !line.contains("Connected") && 
+            if (
+                !line.contains("Disconnected") &&
+                !line.contains("Connected") &&
                 !line.contains("ssh: overflow reading version string") &&
                 !line.contains("disconnect") &&
                 !line.contains("ssh: no auth passed yet, Password authentication failed") &&
@@ -25,33 +32,39 @@ public class FakeSSHParser {
                 !line.contains("[]") &&
                 !line.contains("no auth passed yet") &&
                 !line.contains("invalid packet length") &&
-                !line.contains("no common algorithm") && 
-		!line.contains("connection reset") &&
+                !line.contains("no common algorithm") &&
+                !line.contains("connection reset") &&
                 !line.contains("no route to host")
-            )
-            {
-                System.out.println(line);
-                ArrayList<String> splitLine = new ArrayList<String>(Arrays.asList(line.split("\\s+")));
-		if (line.contains("-OpenSSH")){
+            ) {
+                ArrayList < String > splitLine = new ArrayList < String > (Arrays.asList(line.split("\\s+")));
+                if (splitLine.size() > 4) {
+                    System.out.println(line);
+                    
+                    if (line.contains("-OpenSSH") && splitLine.size() >= 7) {
                         splitLine.set(3, splitLine.get(3) + " " + splitLine.remove(4));
                     }
-                    if(splitLine.size() == 5){
+                    if (splitLine.size() == 5) {
                         splitLine.add("N/A");
-                    } else if (splitLine.size() > 6){
+                    } else if (splitLine.size() > 6) {
                         String password = splitLine.get(5);
 
-                        while(splitLine.size() > 6){
+                        while (splitLine.size() > 6) {
                             password += " " + splitLine.remove(6);
-//			    System.out.println(splitLine.size());
+                            //			    System.out.println(splitLine.size());
+                        }
+                        splitLine.set(5, password);
                     }
-                    splitLine.set(5, password);
-                sshTries.add(new SSHTry(splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3), splitLine.get(4), splitLine.get(5)));
-//                System.out.printf("Date: %s, Time:%s, IP: %s, Software: %s, Username: %s, Password: %s\n", splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3), splitLine.get(4), splitLine.get(5));
+                    SSHTry sshTry = new SSHTry(splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3), splitLine.get(4), splitLine.get(5));
+                    sshTries.add(sshTry);
+                    file.println(sshTry);
+                    //                System.out.printf("Date: %s, Time:%s, IP: %s, Software: %s, Username: %s, Password: %s\n", splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3), splitLine.get(4), splitLine.get(5));
+
                 }
             }
         }
-	System.out.println(sshTries.size());
-	return sshTries.size() != 0;
+        System.out.println(sshTries.size());
+        file.close();
+        return sshTries.size() != 0;
     }
 
     public void writeToFile(String outputFilename) {
--- src/RunFakeSSHParser.java
+++ src/RunFakeSSHParser.java
@@ -1,12 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 public class RunFakeSSHParser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         if (args.length == 0  || args[0].equals("-h") || args[0].equals("--help")) {
             System.out.println("Usage: java RunFakeSSHParser <filename> [<output filename>]");
         }
         else {
             String filename = args[0];
             String outputFilename = args.length > 1 ? args[1] : String.format("%s.csv", filename);
-            FakeSSHParser parser = new FakeSSHParser(filename);
+            FakeSSHParser parser = new FakeSSHParser(filename, outputFilename);
             boolean data = parser.parse();
             if (data && outputFilename != null) {
                 parser.writeToFile(outputFilename);
